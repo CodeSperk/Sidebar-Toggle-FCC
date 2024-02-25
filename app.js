eb4bfd224a680015ca83d277b2ec5d931b81335b
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-02-25 15:51:22 +0600

    added javascript to the buttons

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 
+const toggleBtn = document.querySelector('.toggle-btn');
+const sidebarEl = document.querySelector('.sidebar');
+const sidebarClose = document.getElementById('sidebar-close');
+
+toggleBtn.addEventListener('click', ()=>{
+  // if(sidebarEl.classList.contains('show-sidebar')){
+  //   sidebarEl.classList.remove('show-sidebar');
+  // }else{
+  // sidebarEl.classList.add('show-sidebar');
+  // }
+  sidebarEl.classList.toggle('show-sidebar');
+});
+sidebarClose.addEventListener('click', ()=>{
+  sidebarEl.classList.remove('show-sidebar')
+})
+
+
+
